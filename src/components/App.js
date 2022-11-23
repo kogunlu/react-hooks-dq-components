db022491e367dc3eae7b73fd114f60f957f63b30
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-11-23 09:43:50 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SideMenu from "./SideMenu";
+import MainContent from "./MainContent";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">
+    <SideMenu />
+    <MainContent />
+
+  </div>;
 }
 
 export default App;
